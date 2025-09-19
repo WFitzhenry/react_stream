b288577ea9d660d22272d862ea34756307f46f96
WFitzhenry <warrenfitzhenry22@gmail.com>
2025-09-19 20:11:06 +0100

    update path

--- server.ts
+++ server.ts
@@ -31,6 +31,6 @@ app.listen(PORT, () => {
 // Serve static files from dist
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("/*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "dist/index.html"));
 });
